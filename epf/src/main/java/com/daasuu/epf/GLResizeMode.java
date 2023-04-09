060a37149dfa7aa5b7cf6c708f22047cc387afb2
Anthony <57331729+deflexable@users.noreply.github.com>
2023-04-09 18:04:22 +0100

    Create GLResizeMode.java

---
+++ epf/src/main/java/com/daasuu/epf/GLResizeMode.java
@@ -0,0 +1,11 @@
+package com.daasuu.epf;
+
+
+public enum GLResizeMode {
+    RESIZE_STRETCH,
+    RESIZE_CONTAIN,
+    RESIZE_NONE,
+    RESIZE_COVER,
+    RESIZE_FIT_WIDTH,
+    RESIZE_FIT_HEIGHT
+}
